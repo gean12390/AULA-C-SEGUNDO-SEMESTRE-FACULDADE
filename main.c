efa4041dd0a1b12f0d8619d6a699e63787f90d8e
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2024-08-23 23:34:38 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    int v[] = {1, 4, 0, 1, 4, 0, 1, 2, 3};
+
+    for(i = 0; i < 9; i++){
+        printf("%d \n", v[i]);
+    }
+    
+    return 0;
+}
