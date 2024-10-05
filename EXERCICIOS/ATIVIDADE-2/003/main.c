f2d7da5a9e86a189a596f16fee20ea35def484e7
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2024-10-05 12:54:58 -0300

    Create main.c

---
+++ EXERCICIOS/ATIVIDADE-2/003/main.c
@@ -0,0 +1,22 @@
+// GIOVANNI PASCUOTTE DA SILVA
+// EXERCICIO 6 -- LISTA
+#include <stdio.h>
+
+int soma_num(int a, int b);
+
+int main() {
+
+    printf("\nGIOVANNI PASCUOTTE DA SILVA \n");
+    int a, b;
+
+    printf("Digite 2 Numeros\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("\nA soma dos numeros e igual a %d\n", soma_num(a, b));
+
+    return 0;
+}
+
+int soma_num(int a, int b){
+    return a + b;
+}
